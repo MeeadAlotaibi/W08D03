0ade02a476607025497c82913c9e072b6a061262
MeeadAlotaibi <Meead.Jazi.elq.js@tuwaiq.edu.sa>
2021-11-30 13:58:04 +0300

    connect DB

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 require("dotenv").config();
+require("./db"); //استدعاء للداتا بيس
 
+////////////////////////////////////////////////////
 const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
