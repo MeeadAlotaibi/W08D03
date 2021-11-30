121444f2d3f34a77bcb3caae46144c826765377d
MeeadAlotaibi <Meead.Jazi.elq.js@tuwaiq.edu.sa>
2021-11-30 14:20:25 +0300

    create task Schema

--- db/models/user.js
+++ db/models/user.js
@@ -4,6 +4,7 @@ const user = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, require: true },
   role: [{ type: mongoose.Schema.Types.ObjectId, ref: "Role" }],
+  tasks: [{ type: mongoose.Schema.Types.ObjectId, ref: "Task" }], /// خليت التاسك تابعه لليوزر
 });
 
 module.exports = mongoose.model("User", user);
